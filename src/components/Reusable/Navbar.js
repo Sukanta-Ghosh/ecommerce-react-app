4e0daeb78b31098f148fe4d9abf5fae188fdc6e1
Sukanta Ghosh <48876856+Sukanta-Ghosh@users.noreply.github.com>
2022-05-06 02:45:51 +0530

    Fix unused import error

--- src/components/Reusable/Navbar.js
+++ src/components/Reusable/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import { FaCartArrowDown } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import ReactSwitch from "react-switch";
